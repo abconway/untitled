d46516e17da6b4c71ba3f75948bddf1ab84c2070
Alex Conway <abconway2@gmail.com>
2018-02-10 22:06:07 -0500

    Don't use an autoslugfield
    - The frontend will resolve slug collisions

--- title/models.py
+++ title/models.py
@@ -1,24 +1,5 @@
-from random import choices
-from string import ascii_lowercase
-
 from django.db import models
-from django_extensions.db.models import TimeStampedModel, AutoSlugField
-
-
-class CustomAutoSlugField(AutoSlugField):
-    def slug_generator(self, original_slug, start):
-        yield original_slug
-        for i in range(start, self.max_unique_query_attempts):
-            slug = original_slug
-            random_five_character_string = ''.join(choices(ascii_lowercase, k=5))
-            end = '%s%s' % (self.separator, random_five_character_string)
-            end_len = len(end)
-            if self.slug_len and len(slug) + end_len > self.slug_len:
-                slug = slug[:self.slug_len - end_len]
-                slug = self._slug_strip(slug)
-            slug = '%s%s' % (slug, end)
-            yield slug
-        raise RuntimeError('max slug attempts for %s exceeded (%s)' % (original_slug, self.max_unique_query_attempts))
+from django_extensions.db.models import TimeStampedModel
 
 
 class Title(TimeStampedModel):
@@ -26,4 +7,4 @@ class Title(TimeStampedModel):
         ordering = ['-modified']
 
     name = models.CharField(max_length=4096, null=False, blank=False)
-    slug = CustomAutoSlugField(populate_from='name')
+    slug = models.SlugField(max_length=4096, null=False, blank=False)
